5b1f10c45b2e0daee46d66ab9edd0f32fe6d1c3f
Thiago Santos <thiago.santos@optiisolutions.com>
2024-04-23 16:30:33 -0300

    removing vercel cache

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,11 @@ import type { PageServerLoad } from './$types';
 import { products as api } from '$lib/api/products';
 import type { ProductsQuery } from '$lib/generated/graphql';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async (params) => {
 	const { products } = await api();
-
+	params.setHeaders({
+		'Cache-Control': 'no-cache, no-store, must-revalidate'
+	});
 	return {
 		products: products as ProductsQuery['products']
 	};
